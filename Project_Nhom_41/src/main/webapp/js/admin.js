1a48b7e87aa876201effcc1d8310de356d2af35a
Ngô Thanh Tân <21130529@st.hcmuaf.edu.vn>
2023-11-15 16:36:18 +0700

    chinh sua lai file admin

---
+++ Project_Nhom_41/src/main/webapp/js/admin.js
@@ -0,0 +1,57 @@
+
+let content_home=document.getElementById("home");
+let status_home=content_home.style.visibility='hidden';
+
+let content_product=document.getElementById("product");
+let status_product=content_product.style.visibility='hidden';
+
+
+let content_order=document.getElementById("order");
+let status_order=content_order.style.visibility='hidden';
+
+let content_user=document.getElementById("user");
+let status_user=content_user.style.visibility='hidden';
+function click_home(){
+    if(status_home==="hidden"){
+        content_home.style.transition='opacity 2s';
+        content_home.style.visibility='visible';
+        content_product.style.visibility='hidden';
+        content_order.style.visibility='hidden';
+        content_user.style.visibility='hidden';
+    }else{
+        status_home=content_home.style.visibility='hidden';
+    }
+}
+function click_product(){
+    if(status_product==="hidden"){
+        content_product.style.transition='opacity 2s';
+        content_product.style.visibility='visible';
+        content_order.style.visibility='hidden';
+        content_home.style.visibility='hidden';
+        content_user.style.visibility='hidden';
+    }else{
+        status_product=content_product.style.visibility='hidden';
+    }
+}
+function click_order(){
+    if(status_order==="hidden"){
+        content_order.style.transition='opacity 2s';
+        content_order.style.visibility='visible';
+        content_product.style.visibility='hidden';
+        content_home.style.visibility='hidden';
+        content_user.style.visibility='hidden';
+    }else{
+        status_order=content_order.style.visibility='hidden';
+    }
+}
+function click_user(){
+    if(status_user==="hidden"){
+        content_user.style.transition='opacity 2s';
+        content_user.style.visibility='visible';
+        content_product.style.visibility='hidden';
+        content_order.style.visibility='hidden';
+        content_home.style.visibility='hidden';
+    }else{
+        status_user=content_user.style.visibility='hidden';
+    }
+}
