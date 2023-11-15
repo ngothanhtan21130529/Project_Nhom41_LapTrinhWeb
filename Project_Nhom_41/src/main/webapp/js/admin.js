4eb44dcc40bfa19fb2df21550c0fb3d461116a4b
ngothanhtan21130529 <145730565+ngothanhtan21130529@users.noreply.github.com>
2023-11-15 16:51:41 +0700

    Update admin.js

--- Project_Nhom_41/src/main/webapp/js/admin.js
+++ Project_Nhom_41/src/main/webapp/js/admin.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 let content_home=document.getElementById("home");
 let status_home=content_home.style.visibility='hidden';
@@ -54,60 +54,5 @@ function click_user(){
         content_home.style.visibility='hidden';
     }else{
         status_user=content_user.style.visibility='hidden';
-=======
-let content_trangchu=document.getElementById("trang_chu");
-let status_trangchu=content_trangchu.style.visibility='hidden';
 
-let content_sanpham=document.getElementById("san_pham");
-let status_sanpham=content_sanpham.style.visibility='hidden';
-
-let content_donhang=document.getElementById("don_hang");
-let status_donhang=content_donhang.style.visibility='hidden';
-
-let content_nguoidung=document.getElementById("nguoi_dung");
-let status_nguoidung=content_nguoidung.style.visibility='hidden';
-function click_trangchu(){
-    if(status_trangchu==="hidden"){
-        content_trangchu.style.transition='opacity 2s';
-        content_trangchu.style.visibility='visible';
-        content_sanpham.style.visibility='hidden';
-        content_donhang.style.visibility='hidden';
-        content_nguoidung.style.visibility='hidden';
-    }else{
-        status_trangchu=content_trangchu.style.visibility='hidden';
-    }
-}
-function click_sanpham(){
-    if(status_sanpham==="hidden"){
-        content_sanpham.style.transition='opacity 2s';
-        content_sanpham.style.visibility='visible';
-        content_donhang.style.visibility='hidden';
-        content_trangchu.style.visibility='hidden';
-        content_nguoidung.style.visibility='hidden';
-    }else{
-        status_sanpham=content_sanpham.style.visibility='hidden';
-    }
-}
-function click_donhang(){
-    if(status_donhang==="hidden"){
-        content_donhang.style.transition='opacity 2s';
-        content_donhang.style.visibility='visible';
-        content_sanpham.style.visibility='hidden';
-        content_trangchu.style.visibility='hidden';
-        content_nguoidung.style.visibility='hidden';
-    }else{
-        status_donhang=content_donhang.style.visibility='hidden';
-    }
-}
-function click_nguoidung(){
-    if(status_nguoidung==="hidden"){
-        content_nguoidung.style.transition='opacity 2s';
-        content_nguoidung.style.visibility='visible';
-        content_sanpham.style.visibility='hidden';
-        content_donhang.style.visibility='hidden';
-        content_trangchu.style.visibility='hidden';
-    }else{
-        status_nguoidung=content_nguoidung.style.visibility='hidden';
->>>>>>> c02b210f8ce02761f24631e454539d133dff366e
-    }
 }
