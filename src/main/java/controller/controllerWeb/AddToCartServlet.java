e4a084f0c96c34dcba0c574af369bf0f7437bad9
trantunguyet1 <91519397+trantunguyet1@users.noreply.github.com>
2023-11-29 17:14:08 +0700

    them lai cau truc thu muc

--- src/main/java/controller/controllerWeb/AddToCartServlet.java
+++ src/main/java/controller/controllerWeb/AddToCartServlet.java
@@ -1,4 +1,4 @@
-package controllerWeb;
+package controller.controllerWeb;
 
 import java.io.IOException;
 
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.mysql.cj.Session;
 
 public class AddToCartServlet extends HttpServlet{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
---
+++ src/main/java/controller/controllerWeb/CartController.java
@@ -0,0 +1,5 @@
+package controller.controllerWeb;
+
+public class CartController {
+
+}
