8fbc7e34dd828c4fee763d8fdbace2609f065516
21130530_phamnhuttan <21130530@st.hcmuaf.edu.vn>
2023-11-29 17:46:58 +0700

    sửa file

--- src/main/java/controller/controllerWeb/CartController.java
+++ src/main/java/controller/controllerWeb/CartController.java
@@ -1,5 +1,20 @@
 package controller.controllerWeb;
 
-public class CartController {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
+public class CartController {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String productImage=request.getParameter("product-image");
+        String productName=request.getParameter("product-name");
+        String productPrice=request.getParameter("product-price");
+
+        HttpSession session=request.getSession();
+        session.setAttribute("img", productImage);
+        session.setAttribute("name", productName);
+        session.setAttribute("price", productPrice);
+    }
 }
