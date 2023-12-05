db9be26dc31e36baf5a5b7247b55e7586ae80bc9
21130530_phamnhuttan <21130530@st.hcmuaf.edu.vn>
2023-12-05 12:03:48 +0700

    Cập nhật thêm giao diện admin, thêm phần xử lý sự kiện

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -12,7 +12,7 @@ function changeToCategoriesTab(){
         e.style.display='none';
     });
     document.querySelector('.categories-main-tab').style.display='block';
-    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Danh mục > Loại danh mục';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị danh mục > Loại danh mục';
 }
 function changeToCategoriesAddingTab(){
     let contentTab=document.querySelectorAll('.content-tab');
@@ -22,7 +22,7 @@ function changeToCategoriesAddingTab(){
     document.querySelector('.categories-adding-tab').style.display='block';
     document.querySelector('.categories-adding-tab-general-infomation').style.display='grid';
 
-    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Danh mục > Loại danh mục > Sửa';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị danh mục > Loại danh mục > Sửa';
 }
 function changeToProductTab(){
     let contentTab=document.querySelectorAll('.content-tab');
@@ -30,7 +30,7 @@ function changeToProductTab(){
         e.style.display='none';
     });
     document.querySelector('.product-main-tab').style.display='block';
-    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Danh mục > Sản phẩm';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị danh mục > Sản phẩm';
 }
 function changeToProductAddingTab(){
     let contentTab=document.querySelectorAll('.content-tab');
@@ -38,7 +38,7 @@ function changeToProductAddingTab(){
         e.style.display='none';
     });
     document.querySelector('.product-adding-tab').style.display='block';
-    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Danh mục > Sản phẩm > Sửa';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị danh mục > Sản phẩm > Sửa';
 }
 function changeToOnlineSupportTab(){
     let contentTab=document.querySelectorAll('.content-tab');
@@ -46,5 +46,45 @@ function changeToOnlineSupportTab(){
         e.style.display='none';
     });
     document.querySelector('.online-support-tab').style.display='block';
-    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Danh mục > Sản phẩm > Sửa';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị giao diện > Hỗ trợ trực tuyến';
+}
+function changeToInformationTab(){
+    let contentTab=document.querySelectorAll('.content-tab');
+    contentTab.forEach(e=>{
+        e.style.display='none';
+    });
+    document.querySelector('.information-tab').style.display='block';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị giao diện > Thông tin';
+}
+function changeToOrderListTab(){
+    let contentTab=document.querySelectorAll('.content-tab');
+    contentTab.forEach(e=>{
+        e.style.display='none';
+    });
+    document.querySelector('.order-list').style.display='block';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị thông tin > Danh sách đơn hàng';
+}
+function changeToCustomerContactTab(){
+    let contentTab=document.querySelectorAll('.content-tab');
+    contentTab.forEach(e=>{
+        e.style.display='none';
+    });
+    document.querySelector('.customer-contact').style.display='block';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Quản trị thông tin > Khách hàng liên hệ';
+}
+function changeToUserManagement(){
+    let contentTab=document.querySelectorAll('.content-tab');
+    contentTab.forEach(e=>{
+        e.style.display='none';
+    });
+    document.querySelector('.user-management-tab').style.display='block';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Cấu hình người dùng > Quản lý người dùng';
+}
+function changeToUserInformationTab(){
+    let contentTab=document.querySelectorAll('.content-tab');
+    contentTab.forEach(e=>{
+        e.style.display='none';
+    });
+    document.querySelector('.user-information').style.display='grid';
+    document.querySelector('.homepage-website-text').innerHTML='Trang chủ > Cấu hình người dùng > Thông tin người dùng';
 }
