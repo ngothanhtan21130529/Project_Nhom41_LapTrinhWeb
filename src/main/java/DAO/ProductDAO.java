71dbe703545a7a23dd76e12fcfa64b5110adddf1
21130530_phamnhuttan <21130530@st.hcmuaf.edu.vn>
2023-12-12 16:11:36 +0700

    chỉnh sửa 1 số file

--- src/main/java/DAO/ProductDAO.java
+++ src/main/java/DAO/ProductDAO.java
@@ -14,129 +14,32 @@ public class ProductDAO implements DAOInterface<Product> {
     public static ProductDAO getInstance(){
         return new ProductDAO();
     }
+
     @Override
-    public int insert(Product product) {
-        int res=0;
-        try {
-            Connection con = MySqlConnection.getConnection();
-            Statement st = con.createStatement();
-            String sql = "insert into products (id, categories_id, img_id, name, available, price, status, create_at, update_at"+
-                    "value('"+product.getId()+"', '"+product.getCategoryID()+"', '"+product.getThumbnail()+"', '"
-                    +product.getProductName()+"', '"+product.getPrice()+"', '"+product.getStatus()+"')''";
-            res=st.executeUpdate(sql);
-
-            System.out.println("Execute querry success: "+sql);
-            System.out.println("There "+res+" line changed!");
-
-            MySqlConnection.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
+    public int insert(Product product) throws SQLException {
+        return 0;
     }
 
     @Override
     public int update(Product product) {
-        int res=0;
-        try {
-            Connection con = MySqlConnection.getConnection();
-            Statement st = con.createStatement();
-            String sql = "update products"+
-                    "set"+
-                    " category_id='"+product.getCategoryID()+"'"+
-                    ", thumbnail='"+product.getThumbnail()+"'"+
-                    ", name='"+product.getProductName()+"'"+
-                    ", price='"+product.getPrice()+"'"+
-                    ", status='"+product.getStatus()+"'"+
-                    " where id='"+product.getId()+"\'";
-            res=st.executeUpdate(sql);
-
-            System.out.println("Execute querry success: "+sql);
-            System.out.println("There "+res+" line changed!");
-
-            MySqlConnection.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
+        return 0;
     }
 
     @Override
     public int delete(Product product) {
-        int res=0;
-        try {
-            Connection con = MySqlConnection.getConnection();
-            Statement st = con.createStatement();
-            String sql = "delete from products"+
-                    " where id='"+product.getId()+"'";
-            res=st.executeUpdate(sql);
-
-            System.out.println("Execute querry success: "+sql);
-            System.out.println("There "+res+" line changed!");
-
-            MySqlConnection.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
+        return 0;
     }
 
     @Override
     public Product selectById(Product product) {
-        Product res=null;
-        try {
-            Connection con = MySqlConnection.getConnection();
-            Statement st = con.createStatement();
-            String sql = "select * from products where id='"+product.getId()+"'";
-            System.out.println(sql);
-            ResultSet rs =st.executeQuery(sql);
-
-            while (rs.next()){
-                int id=rs.getInt("id");
-                int categoryId=rs.getInt("category_id");
-                String name=rs.getString("name");
-                int price=rs.getInt("price");
-                String status=rs.getString("status");
-                String imageURL=rs.getString("thumbnail");
-
-                res=new Product(id, categoryId, name, price, status, imageURL);
-            }
-
-            MySqlConnection.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
+        return null;
     }
 
     @Override
     public ArrayList<Product> selectByCondition(String condition) {
-        ArrayList<Product> res=new ArrayList<>();
-        try {
-            Connection con = MySqlConnection.getConnection();
-            Statement st = con.createStatement();
-            String sql = "select * from products where"+condition;
-            System.out.println(sql);
-            ResultSet rs =st.executeQuery(sql);
-
-            while (rs.next()){
-                int id=rs.getInt("id");
-                int categoryId=rs.getInt("category_id");
-                String name=rs.getString("name");
-                int price=rs.getInt("price");
-                String status=rs.getString("status");
-                String imageURL=rs.getString("thumbnail");
-
-                Product product=new Product(id, categoryId, name, price, status, imageURL);
-                res.add(product);
-            }
-
-            MySqlConnection.getConnection().close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
+        return null;
     }
+
     public int count(String txtSearch){
         try {
             String querry="select count(products.id) from products where products.product_name like ?";
