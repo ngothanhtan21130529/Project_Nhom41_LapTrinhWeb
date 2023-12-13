f22c2b7282a20c1f40b886a8e5d8be7b9b0a1509
Ngô Thanh Tân <21130529@st.hcmuaf.edu.vn>
2023-12-13 20:22:16 +0700

    up cac file cart,cartcontroller,cartquantity,cartremove

--- src/main/java/controller/controllerWeb/CartController.java
+++ src/main/java/controller/controllerWeb/CartController.java
@@ -1,5 +1,6 @@
 package controller.controllerWeb;
 
+import model.Cart;
 import model.Product;
 
 import javax.servlet.RequestDispatcher;
@@ -17,6 +18,28 @@ public class CartController {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String img = request.getParameter("img");
+        String name = request.getParameter("name");
+        int price = Integer.parseInt(request.getParameter("price"));
+        Product product=new Product(img,name,price);
+        HttpSession session = request.getSession();
+        if (session != null) {
+            for(int i=0;i<Cart.products.size();i++){
+                if(Cart.products.size()==0){
+                    Cart.addProduct(product);
+                    break;
+                }else if(Cart.products.get(i).equals(product)){
+                    Cart.addProduct(product);
+                    break;
+                }
 
+            }
+
+            session.setAttribute("list",Cart.products);
+            RequestDispatcher requestDispatcher = session.getServletContext().getRequestDispatcher("index.jsp");
+            requestDispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("index.jsp");
+        }
     }
 }
--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -1,11 +1,22 @@
 package model;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Cart implements Serializable {
-    ArrayList<Product> products=new ArrayList<>();
-    public void addProduct(Product product){
+   public static  ArrayList<Product> products=new ArrayList<>();
+    public static ArrayList<Product> addProduct(Product product){
         products.add(product);
+        return products;
+    }
+    public static ArrayList<Product> deleteProduct(Product product){
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).equals(product)){
+                products.remove(i);
+                break;
+            }
+        }
+        return products;
     }
 }
