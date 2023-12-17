adaaeeafcb3ec98422a71c282a03ef45a22e8361
Ngô Thanh Tân <21130529@st.hcmuaf.edu.vn>
2023-12-17 18:24:51 +0700

    up file CommentController,commentDAO

---
+++ src/main/java/controller/controllerWeb/CommentController.java
@@ -0,0 +1,29 @@
+package controller.controllerWeb;
+
+import DAO.CommentDAO;
+import model.Comment;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class CommentController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String comment=req.getParameter("comment");
+        try {
+            CommentDAO.getInstance().insertComment(new Comment(1,1,1,null,null));
+            resp.sendRedirect("");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
