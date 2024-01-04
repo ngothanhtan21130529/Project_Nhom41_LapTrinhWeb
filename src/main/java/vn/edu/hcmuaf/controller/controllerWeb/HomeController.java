98d12e7dbbdc3c1d00db92b4a755a9ae52d7b25a
trantunguyet1 <91519397+trantunguyet1@users.noreply.github.com>
2024-01-04 17:30:58 +0700

    commit

--- src/main/java/vn/edu/hcmuaf/controller/controllerWeb/HomeController.java
+++ src/main/java/vn/edu/hcmuaf/controller/controllerWeb/HomeController.java
@@ -1,5 +1,8 @@
 package vn.edu.hcmuaf.controller.controllerWeb;
 
+import vn.edu.hcmuaf.model.Category;
+import vn.edu.hcmuaf.service.CategoryService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +10,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
+
+    private CategoryService categoryService;
+
+    public HomeController() throws SQLException {
+        categoryService = new CategoryService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ArrayList<Category> categories = categoryService.getAllCategories();
+
+        req.setAttribute("categoryList", categories);
+
+
         req.getRequestDispatcher("/views/index.jsp").forward(req, resp);
     }
 
