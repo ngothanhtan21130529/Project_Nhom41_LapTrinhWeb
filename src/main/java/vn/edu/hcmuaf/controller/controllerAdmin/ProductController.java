1229c766fbc09b6ff27ad29fb27d9d82fe6503ff
trantunguyet1 <91519397+trantunguyet1@users.noreply.github.com>
2024-01-08 02:58:21 +0700

    commit

--- src/main/java/vn/edu/hcmuaf/controller/controllerAdmin/ProductController.java
+++ src/main/java/vn/edu/hcmuaf/controller/controllerAdmin/ProductController.java
@@ -4,67 +4,32 @@ import vn.edu.hcmuaf.dao.ProductDAO;
 import vn.edu.hcmuaf.model.Product;
 import vn.edu.hcmuaf.service.ProductService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
-//@WebServlet("/products")
-//public class ProductController extends HttpServlet {
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ProductDAO productDAO = new ProductDAO();
-//        ArrayList<Product> products = productDAO.getListProduct();
-//        request.setAttribute("products", products);
-//
-//        request.getRequestDispatcher("jewelrys-list.jsp").forward(request, response);
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String productName = request.getParameter("products");
-//    response.sendRedirect(request.getContextPath() + "/products");
-//    }
-//}
 
-//@WebServlet("/products")
-//public class ProductController extends HttpServlet {
-//    private ProductDAO productDAO;
-//
-//    @Override
-//    public void init() throws ServletException {
-//        super.init();
-//        try {
-//            productDAO = new ProductDAO();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ArrayList<Product> products = productDAO.getListProduct();
-//        request.setAttribute("products", products);
-//
-//        request.getRequestDispatcher("products.jsp").forward(request, response);
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        // Xử lý các yêu cầu POST nếu cần
-//        response.sendRedirect(request.getContextPath() + "/products");
-//    }
 @WebServlet("/products")
 public class ProductController extends HttpServlet {
-    private ProductService productService;
 
+    private ProductService productService = new ProductService();
+
+    public ProductController() throws SQLException {
+        productService = new ProductService();
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ArrayList<Product> products = productService.getListProducts();
-        request.setAttribute("products", products);
+        request.setAttribute("productList", products);
 
-        request.getRequestDispatcher("products.jsp").forward(request, response);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Xử lý các yêu cầu POST nếu cần
-        response.sendRedirect(request.getContextPath() + "/products");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/views/web/category/products.jsp");
+        dispatcher.forward(request, response);
     }
 }
+
